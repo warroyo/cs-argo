3b230a8b4017e03404a5136ebe474f4f299665aa
Will Arroyo <warroyo7199008@gmail.com>
2021-08-03 10:43:14 -0600

    add apply for initial app

--- scripts/install-argo.sh
+++ scripts/install-argo.sh
@@ -13,5 +13,7 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl patch serviceaccount argocd-redis -p '{"imagePullSecrets": [{"name": "dockerhub"}]}' -n argocd
 #patch to get a loadbalancer
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-k scale deployment argocd-redis --replicas 0 -n argocd
-k scale deployment argocd-redis --replicas 1 -n argocd
+kubectl scale deployment argocd-redis --replicas 0 -n argocd
+kubectl scale deployment argocd-redis --replicas 1 -n argocd
+#apply initial app
+kubectl apply -f ../argo/initial-app.yml
