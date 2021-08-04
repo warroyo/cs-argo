5c6b01b5e6ef9712a8e6308b3124a993637e5f3e
Will Arroyo <warroyo7199008@gmail.com>
2021-08-04 12:39:37 -0600

    fix env vars

--- scripts/prep-secrets.sh
+++ scripts/prep-secrets.sh
@@ -3,4 +3,4 @@
 #purpose of this script is to add any secrets that need to exist for initial install of components
 # if using an external secret manager this could be used for one time config
 
-ytt -v vrlic_api_token=$VRLIC_TOKEN -v docker_pass=$DOCKER_PASS -v docker_user=$DOCKER_USER -f manifests/secrets.yml | kubectl apply -f -
+ytt -v vrlic_api_token=$VRLIC_TOKEN -v docker_pass=$DOCKERHUB_PASS -v docker_user=$DOCKERHUB_USER -f manifests/secrets.yml | kubectl apply -f -
